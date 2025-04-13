21d3214bbcb64730053b3704e25de7836183519c
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-04-13 15:07:17 +0700

    feat(seeder): update test user details in DatabaseSeeder
    Modified the DatabaseSeeder to change the test user's name and email address. The new details are now 'Ragil Pamungkas' and 'test@sipamungkas.com', respectively, ensuring the seeder reflects accurate test data for development purposes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Ragil Pamungkas',
+            'email' => 'test@sipamungkas.com',
             'password'=> bcrypt('qwerty123'),
         ]);
 
