648a90b73bbb52d5b846d38bcf636f18129e07bc
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-04-13 14:11:26 +0700

    feat(logo): add logo image to the dashboard
    Introduced a new logo image file and updated the AppLogo component to display the logo instead of the previously commented-out AppLogoIcon. This enhancement improves the visual branding of the dashboard.

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,9 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                {/* <AppLogoIcon className="size-5 fill-current text-white dark:text-black" /> */}
+                <img src="/logo.png" className="" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">Chentil Dashboard</span>
