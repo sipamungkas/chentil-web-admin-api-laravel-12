87a0957caad9a43a45297afb1c84b160e9b64710
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-04-14 15:46:16 +0700

    fix(app-sidebar): comment out NavFooter component in sidebar
    Temporarily commented out the NavFooter component in the AppSidebar to address layout issues. This change allows for further adjustments without affecting the current user interface.

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -116,7 +116,7 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
+                {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
